047f7ba3e9e7e6c119036d267f5771abbb823ecd
camara <csouleymane025@gmail.com>
2021-05-16 03:00:04 +0100

    init

--- competence_service.js
+++ competence_service.js
@@ -30,21 +30,37 @@ window.addEventListener("load",()=>{
 
     const a=document.querySelectorAll(".select");
     const ul=document.querySelectorAll(".exp_child");
+    const image=document.querySelectorAll(".image");
     
    
     a.forEach((value,index)=>{
         value.addEventListener("click",()=>{
             if(value.checked){
+
                 ul.forEach(v=>{
                     v.style.opacity=0;
                 })
+
                 ul[index].style.opacity=1;
+                image.forEach(val=>{
+                    val.style.width="100%";
+                    val.style.height="100%";
+                    val.style.top="0px";
+                    val.style.left="0px";
+                    val.style.transform="initial";
+               });
+               image[index].style.width=`${150}%`;
+               image[index].style.height=`${150}%`;
+               image[index].style.top=`${50}%`;
+               image[index].style.left=`${50}`;
+               image[index].style.transform="translate(-0%,-50%)";
+               image[index].style.transition="all 0.5s";
             }
         });
+
     })
     
   
 })
-
 
 
